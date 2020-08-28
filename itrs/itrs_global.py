519bf8823d50c5ec0006f1fd675b0fd090925eb6
conta16 <luca.contalbo@gmail.com>
2020-08-28 15:55:26 +0200

    This reverts commit eb544c93811e7b1d75931fb55e784ffff46d3b1f.

--- itrs/itrs_global.py
+++ itrs/itrs_global.py
@@ -5,10 +5,11 @@ from vyper.itrs.koat import KOAT
 class ITRS_global:
 
 	def __init__(self, procedure):
-		self.state_vars, self.functions, self.structs = self.get_globals(procedure)
+		self.state_vars, self.state_types, self.functions, self.structs = self.get_globals(procedure)
 		self.itrs = [] #itrs will have the dicts of the parsed whiles in each function
+		print(self.state_vars)
 		for i in self.functions:
-			self.itrs.append(ITRS(i, self.state_vars.copy(), self.structs.copy()).run())
+			self.itrs.append(ITRS(i, self.state_vars.copy(), self.state_types.copy(), self.structs.copy()).run())
 		print(self.itrs)
 
 		self.itrs = KOAT().get_bounds(self.itrs)
@@ -20,16 +21,22 @@ class ITRS_global:
 
 	def get_globals(self, procedure):
 		vars = {}
+		types = {}
 		functions = []
 		structs = {}
 
 		for obj in procedure:
 			if obj['ast_type'] == "Assign" or obj['ast_type'] == "AnnAssign":
 				vars["self." + obj['target']['id']] = "self." + obj['target']['id']
+				types["self." + obj['target']['id']] = obj['target']['ast_type']
 			elif obj['ast_type'] == "FunctionDef":
 				functions.append(obj)
 			elif obj['ast_type'] == "ClassDef" and obj['class_type'] == "struct":
 				structs[obj['name']] = []
 				for i in obj['body']:
-					structs[obj['name']].append(i['target']['id'])
-		return vars, functions, structs
+					structs[obj['name']].append([i['target']['id'], i['annotation']['id']])
+		print(vars)
+		print(types)
+		print(functions)
+		print(structs)
+		return vars, types, functions, structs
